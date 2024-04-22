710f729e9741e26be8e5c596ab7ec8bff21940bc
Student <Student@Mac-mini-4.local>
2024-04-22 11:22:01 +0530

    message

---
+++ IOS_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS_Project
+//
+//  Created by Student on 16/04/24.
+//  Copyright © 2024 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
